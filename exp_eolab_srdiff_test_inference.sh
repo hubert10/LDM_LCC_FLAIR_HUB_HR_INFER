edd86b6080448d816a2831cdd61a9a5b3b6ade50
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2026-03-31 16:59:35 +0200

    first commit

--- exp_eolab_srdiff_test_inference.sh
+++ exp_eolab_srdiff_test_inference.sh
@@ -19,4 +19,4 @@ export DATA_DIR
 source /home/eouser/flair_venv/bin/activate
 which python
 cd $HOME/exp_2026/LDM_LCC_FLAIR_HUB_HR_INFER
-python trainer.py --config_file=./configs/train_main/ --exp_name misr/srdiff_maxvit_ltae_ckpt --hparams="diff_net_ckpt=/my_data/Results/LDM_LCC_FLAIR_HUB_HR_INFER/checkpoints/srdiff_maxvit_ltae_ckpt" --infer
+python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --hparams="diff_net_ckpt=/my_data/Results/LDM_LCC_FLAIR_HUB_HR_INFER/checkpoints/srdiff_maxvit_ltae_ckpt" --infer
--- exp_eolab_srdiff_train_backbone.sh
+++ exp_eolab_srdiff_train_backbone.sh
@@ -19,4 +19,4 @@ export DATA_DIR
 source /home/eouser/flair_venv/bin/activate
 which python
 cd $HOME/exp_2026/LDM_LCC_FLAIR_HUB_HR_INFER
-python trainer.py --config_file=./configs/train_main/ --exp_name misr/srdiff_maxvit_ltae_ckpt --reset
+python trainer.py --config_file=./configs/train_main/ --exp_name srdiff_maxvit_ltae_ckpt --reset
